60a355034a7aa334b22918c40daf9d12d8b01cf0
wxf <wxf_tensor@163.com>
2022-11-03 17:49:34 +0800

    feat: html-webpack-plugin

--- build/dev.js
+++ build/dev.js
@@ -1,12 +1,20 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './src/app/index.js',
   output: {
     filename: 'main.js',
+    clean: true,
     path: path.resolve(__dirname, '../dist')
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src/app/index.html'),
+      filename: path.resolve(__dirname, '../dist/index.html')
+    }),
+  ],
   module: {
     rules: [
       {
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # 依赖
 ``` bash
-npm install -D webpack webpack-cli stylus stylus-loader css-loader style-loader @swc/core swc-loader
+npm install -D webpack webpack-cli stylus stylus-loader \
+  css-loader style-loader @swc/core swc-loader \
+  html-webpack-plugin
 npm install react react-dom
 ```
