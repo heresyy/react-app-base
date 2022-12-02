427f73e2bae7d2e46a95052942de6c97e46b1e56
关于 <372301467@qq.com>
2022-12-02 02:19:29 +0000

    update build/dev.js.
    Signed-off-by: 关于 <372301467@qq.com>

--- build/dev.js
+++ build/dev.js
@@ -5,6 +5,17 @@ module.exports = {
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
+    
+    // 代理服务器
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://18.143.135.109:8090/',
+    //     pathRewrite: { '^/api': '' },
+    //   }
+    // },
+
+    // BrowserRouter
+    // historyApiFallback: true
   },
   ...config
 }
