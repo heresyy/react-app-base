f11c676336920c0935d860949f5f1edd4c4a53c1
gy <372301467@qq.com>
2023-01-08 16:47:36 +0800

    chore: chore

--- src/cmp/layout/index.js
+++ src/cmp/layout/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './index.styl'
 
 export default
 function Layout({ children }) {
-  return <div>
-    <header>this is header</header>
+  return <div className = 'page'>
+    <header>
+      <Link to = '/'>Home</Link> | <Link
+        to = '/another'>another</Link>
+    </header>
     {children}
-    <footer>this is footer</footer>
+    <footer>@ppzp/react-demo</footer>
   </div>
 }
---
+++ src/page/another/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Layout from '../../cmp/layout'
+
+export default
+function AnotherPage() {
+  return <Layout>
+    <h1>another</h1>
+  </Layout>
+}
